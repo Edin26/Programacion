687081a9bbdbdb45b6c137bb61669f7d25dc122a
Edin26 <edinpop25@gmail.com>
2022-11-01 23:22:53 +0100

    palabra
    boletin2

---
+++ Boletin2/PopPopRodolfoEdin/Ejercicio1/src/CadenaPalíndroma.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class CadenaPalíndroma {
+
+    public static void main(String[] args) {
+
+        String fraseAComprobar = null;
+        String fraseTemp = "";
+        boolean esPalindroma = false;
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingresa la frase : ");
+        fraseAComprobar = entrada.nextLine().trim().toUpperCase();
+
+        //Quitar los espacios si la frase los tuviera
+        var fraseSinEspacios = fraseAComprobar.replace(" ","");
+        //longitud frase sin espacios
+        var longitud = fraseSinEspacios.length();
+        //Invertir la palabra y almacenar
+        for (int i = longitud ; i >0; i --){
+            fraseTemp += fraseSinEspacios.charAt(i-1);
+        }
+        //comprobacion palabra palindroma
+        if(fraseSinEspacios.equals(fraseTemp)){
+            System.out.println("La palabra es palindroma");
+        }
+        else {
+            System.out.println("La palabra no es palindroma");
+        }
+    }
+}
