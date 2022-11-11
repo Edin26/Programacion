48fbcbb30ecd0fe9bf03b932a3673497d7f3a334
Edin26 <edinpop25@gmail.com>
2022-11-11 20:20:37 +0100

    bol 3

--- Boletin3/PopPopRodolfoEdin/src/bloqueEjercicios1/MTDNumeros.java
+++ Boletin3/PopPopRodolfoEdin/src/bloqueEjercicios1/MTDNumeros.java
@@ -15,18 +15,106 @@ public class MTDNumeros {
             return false;
         }
     }
-
-    public static int MinimoComunMultiplo(int num1,int num2,int num3){
-
+    public static int MinimoComunMultiplo(int num1, int num2, int num3) {
+        boolean encontrado = false;
         int mcm = 0;
-        for (int i = 1 ; i < 100;i++){
-            if(  i % num1  == 0 && i % num2  == 0  && i % num3  == 0  ){
-             mcm = i;
-             break;
-            }
-        }
+        //Asignarle el maximo de los 3 numeros
+        mcm = Math.max(Math.max(num1, num2), num3);
 
+        do {
+            if (mcm % num1 == 0 && mcm % num2 == 0 && mcm % num3 == 0) {
+                encontrado = true;
+                break;
+            }
+            mcm++;
+
+        } while (!encontrado);
         return mcm;
     }
+    public static float CalculaResistenciaEquivalente(float r1, float r2, int... unidades) {
+        float resistenciaEquivalente = 0;
+        int unidad = 0;
+        //comprobar si el parametro opcional no esta vacio
+        if (unidades.length > 0) {
+            unidad = unidades[0];
+        }
+
+        //Calculo resistencia equivalente
+        resistenciaEquivalente = (r1 * r2) / (r1 + r2);
+
+        //si se le pasa parametro opcional
+        if (unidad != 0) {
+            switch (unidad) {
+                case 1:
+                    //Si unidades vale 1 : En microOhmnios (10'6 ohmnios)
+                    resistenciaEquivalente = resistenciaEquivalente * 1000000;
+                    break;
+                case 2:
+                    //Si unidades vale 2: En Kiloohmnios (10-3 ohmnios)
+                    resistenciaEquivalente = (float) (resistenciaEquivalente * 0.001);
+                    break;
+            }
+        }
+        return resistenciaEquivalente;
+    }
+    public static void AlmacenarMediaPositivos(int[] array) {
+        double mediaPositivos = 0;
+
+        boolean completado = false;
+
+        do{
+            completado = true;
+            int contadorPositivos = 0;
+            double suma = 0;
+            //Calcula la media de positivos
+            for (var valor : array) {
+                if (valor > 0) {
+                    suma += valor;
+                    contadorPositivos++;
+                }
+            }
+            mediaPositivos = suma / contadorPositivos;
+            //Buscar negativos
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] < 0) {
+                    array[j] = (int) Math.ceil(mediaPositivos);
+                    completado = false;
+                    break;
+                }
+            }
+            //comprobar que todos los valores array son positivos
+/*            for (var valor : array) {
+                if (valor < 0) {
+
+                }
+            }*/
+        }
+        while (!completado);
+    }
+    public static float CalculaMedia(float matriz[]) {
+        float media = 0;
+        float suma = 0;
+
+        for (var valor : matriz) {
+            suma += valor;
+        }
+        media = (float) (suma / matriz.length);
+        media = (float) (Math.round(media * 100d) / 100d);
+        return media;
+    }
+    public static float CalculaMedia(int matriz[]) {
+        float media = 0;
+        float suma = 0;
+
+        for (var valor : matriz) {
+            suma += valor;
+        }
+        media = (float) (suma / matriz.length);
+        media = (float) (Math.round(media * 100d) / 100d);
+        return media;
+
+    }
+
+
 
 }
--- Boletin3/PopPopRodolfoEdin/src/pruebas/PrincipalBloque1.java
+++ Boletin3/PopPopRodolfoEdin/src/pruebas/PrincipalBloque1.java
@@ -2,6 +2,8 @@ package pruebas;
 
 import bloqueEjercicios1.MTDNumeros;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 
@@ -20,13 +22,15 @@ public class PrincipalBloque1 {
             System.out.println();
             System.out.println("1. Numero perfecto. ");
             System.out.println("2. Minimo comun multiplo. ");
+            System.out.println("3. Resistencia equivalente. ");
+            System.out.println("4. Modifica todas aquellas posiciones que almacenan un número negativo ");
+            System.out.println("5. Calcula la media de una matriz.");
 
             try {
                 opcionElejida = entrada.nextInt();
-            }catch (Exception e){
+            } catch (Exception e) {
                 System.out.println("Opcion no valida.");
             }
-
             switch (opcionElejida) {
                 case 1:
                     //region Numero Perfecto
@@ -38,30 +42,70 @@ public class PrincipalBloque1 {
                         System.out.println("Solo se permiten numeros.");
                     }
                     if (numero > 0) {
-                        System.out.println("El numero es perfecto ? "+ MTDNumeros.NumeroPerfecto(numero));
+                        System.out.println("El numero es perfecto ? " + MTDNumeros.NumeroPerfecto(numero));
                     }
                     //endregion
                     break;
                 case 2:
-                    int num1,num2,num3=0;
-                    System.out.println("Ingresa el primer numero : ");
-                    num1 = entrada.nextInt();
-                    System.out.println("Ingresa el segundo numero : ");
-                    num2 = entrada.nextInt();
-                    System.out.println("Ingresa el tercer numero : ");
-                    num3 = entrada.nextInt();
-
-                    System.out.println("Resultado : "+MTDNumeros.MinimoComunMultiplo(num1,num2,num3));
-
-
+                    //region Minino Comun Multiplo
+                    int num1 = 0, num2 = 0, num3 = 0;
+                    boolean numerosValidos = false;
+                    while (!numerosValidos) {
+                        try {
+                            System.out.println("Ingresa el primer numero mayor que 0 y menor que 100 : ");
+                            num1 = entrada.nextInt();
+                            System.out.println("Ingresa el segundo numero mayor que 0 y menor que 100 : ");
+                            num2 = entrada.nextInt();
+                            System.out.println("Ingresa el tercer numero mayor que 0 y menor que 100 : ");
+                            num3 = entrada.nextInt();
+                        } catch (Exception e) {
+                            System.out.println("El numero tiene que ser un entero");
+                        }
+                        if ((num1 > 0 && num1 < 100) && (num2 > 0 && num2 < 100) && (num3 > 0 && num3 < 100)) {
+                            numerosValidos = true;
+                        } else {
+                            System.out.println("Los numeros no estan en el rango");
+                        }
+                        entrada.nextLine();
+                    }
+                    System.out.println("El minimo comun multiplo es : " + MTDNumeros.MinimoComunMultiplo(num1, num2, num3));
+                    //endregion
                     break;
                 case 3:
-
+                    //region Resistencia equivalente
+                    float r1=5;
+                    float r2=30;
+                    System.out.println("Resistencia equivalente : "+MTDNumeros.CalculaResistenciaEquivalente(r1,r2)+" Ohmnios.");
+                    System.out.println("Resistencia equivalente : "+ MTDNumeros.CalculaResistenciaEquivalente(r1,r2,1)+" microOhmnios.");
+                    System.out.println("Resistencia equivalente : "+MTDNumeros.CalculaResistenciaEquivalente(r1,r2,2)+" KiloOhmnios.");
+                    //endregion
                     break;
                 case 4:
-
+                    int [] array = {100,3,9,-8,4,-90,-7};
+                    System.out.println("Array antes de llamar metodo modificar posiciones negativas ");
+                    System.out.println(Arrays.toString(array));
+                    MTDNumeros.AlmacenarMediaPositivos(array);
+                    System.out.println("Array antes de llamar metodo modificar posiciones negativas ");
+                    System.out.println(Arrays.toString(array));
                     break;
                 case 5:
+                    //region Calcula media
+                    Random rnd = new Random();
+                    float matrizTipoFloat []= new float[10];
+                    int matrizTipoInt []= new int[10];
+
+                    for (int i =0; i<matrizTipoFloat.length;i++){
+                        matrizTipoFloat[i] = rnd.nextFloat(1,100);
+                    }
+                    for (int i =0; i<matrizTipoInt.length;i++){
+                        matrizTipoInt[i] = rnd.nextInt(1,100);
+                    }
+                    System.out.println("La media de matriz de float es : "+ MTDNumeros.CalculaMedia(matrizTipoFloat) );
+                    System.out.println("La media de matriz de int es : "+ MTDNumeros.CalculaMedia(matrizTipoInt) );
+
+
+
+                    //endregion
                     break;
                 case 6:
                     break;
